ca0f4dddbb77c27d6ceec57339b20427031102f8
kameronyork <thekameronyork99@gmail.com>
2023-12-14 22:37:51 -0700

    added functionality to check if the verse ids are hidden every second.  Now the function waits until every button the page has been loaded before resuming the interval.  This will allow the page to be fully loaded before the check starts again.  Previously a timer had to be set long enough for the buttons to load in completely.  This is tough because sometimes it taks <1 second and sometimes it takes >15 seconds.  This way removes that time guessing!

--- scripting/chrome_extension/backup.js
+++ scripting/chrome_extension/backup.js
@@ -124,14 +124,13 @@ function createSpace() {
 }
 
 
-async function replaceVerseNumbersWithButtons() { 
+async function replaceVerseNumbersWithButtons(callback) {
   const verseNumbers = document.querySelectorAll('.verse-number');
   const url = window.location.href;
   const langIndex = url.indexOf('lang=eng');
 
   if (langIndex > -1) {
-    verseNumbers.forEach(async (verseNumber) => {
-      // Creating the scripturePath for lookup
+    const promises = Array.from(verseNumbers).map(async (verseNumber) => {
       const chapterIndex = url.lastIndexOf('/', langIndex - 1);
       const bookIndex = url.lastIndexOf('/', chapterIndex - 1);
 
@@ -140,21 +139,18 @@ async function replaceVerseNumbersWithButtons() {
         chapter = chapter.split('?')[0];
       }
 
-      const bookAbbr = url.substring(bookIndex + 1, chapterIndex);  // Getting the Book
-      const verseNumberText = verseNumber.textContent.trim();  // Getting the Verse Number
-      const bookFullName = bookDecoder[bookAbbr] || '';  // Decoding the Book
-      const scripturePath = `${bookFullName} ${chapter}:${verseNumberText}`;  // Creating the Path for Lookup
+      const bookAbbr = url.substring(bookIndex + 1, chapterIndex);
+      const verseNumberText = verseNumber.textContent.trim();
+      const bookFullName = bookDecoder[bookAbbr] || '';
+      const scripturePath = `${bookFullName} ${chapter}:${verseNumberText}`;
 
-      // Getting the count of times quoted:
       const jsonData = await fetchJSON('https://kameronyork.com/datasets/conference-quotes.json');
       const scriptureCount = countScriptureInstances(jsonData, scripturePath);
       const matchingEntries = getEntriesWithScripture(jsonData, scripturePath);
 
-      // Retrieve the saved color from chrome.storage.sync
-      chrome.storage.sync.get('buttonColor', function(data) {
+      chrome.storage.sync.get('buttonColor', function (data) {
         const savedColor = data.buttonColor;
 
-        // Defining the width of the buttons based on the number of digits in the variable:
         let verseButtonWidth = '25px';
         let countButtonWidth = '25px';
 
@@ -174,186 +170,220 @@ async function replaceVerseNumbersWithButtons() {
           countButtonWidth = '35px';
         }
 
-      if (scriptureCount === 0) {
-        const verseButton = document.createElement('button');
-        verseButton.style.width = verseButtonWidth;
-        verseButton.style.height = '20px';
-        verseButton.style.border = 'none';
-        verseButton.style.fontSize = '12px';
-        verseButton.textContent = verseNumberText;
-        verseButton.style.background = savedColor || '#191970';
-        verseButton.style.color = 'white';
-        verseButton.style.borderRadius = '5px';
-        verseButton.style.display = 'inline-block';
-        verseButton.style.textAlign = 'center';
-        verseButton.style.lineHeight = '20px';
+        if (scriptureCount === 0) {
+          const verseButton = document.createElement('button');
+          verseButton.style.width = verseButtonWidth;
+          verseButton.style.height = '20px';
+          verseButton.style.border = 'none';
+          verseButton.style.fontSize = '12px';
+          verseButton.textContent = verseNumberText;
+          verseButton.style.background = savedColor || '#191970';
+          verseButton.style.color = 'white';
+          verseButton.style.borderRadius = '5px';
+          verseButton.style.display = 'inline-block';
+          verseButton.style.textAlign = 'center';
+          verseButton.style.lineHeight = '20px';
 
-        verseButton.addEventListener('click', async function() {
-          const overlay = document.createElement('div');
-          overlay.style.position = 'fixed';
-          overlay.style.top = '0';
-          overlay.style.left = '0';
-          overlay.style.width = '100%';
-          overlay.style.height = '100%';
-          overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-          overlay.style.display = 'flex';
-          overlay.style.alignItems = 'center';
-          overlay.style.justifyContent = 'center';
-          overlay.style.zIndex = '9999';
-        
-          const scriptureDetails = document.createElement('div');
-          scriptureDetails.style.backgroundColor = savedColor || '#191970';
-          scriptureDetails.style.color = 'white';
-          scriptureDetails.style.padding = '20px';
-          scriptureDetails.style.borderRadius = '10px';
-          scriptureDetails.textContent = 'No entries found';
-        
-          const closeButton = document.createElement('button');
-          closeButton.textContent = 'Close';
-          closeButton.addEventListener('click', function() {
-            overlay.remove();
+          verseButton.addEventListener('click', async function() {
+            const overlay = document.createElement('div');
+            overlay.style.position = 'fixed';
+            overlay.style.top = '0';
+            overlay.style.left = '0';
+            overlay.style.width = '100%';
+            overlay.style.height = '100%';
+            overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+            overlay.style.display = 'flex';
+            overlay.style.alignItems = 'center';
+            overlay.style.justifyContent = 'center';
+            overlay.style.zIndex = '9999';
+
+            const scriptureDetails = document.createElement('div');
+            scriptureDetails.style.backgroundColor = savedColor || '#191970';
+            scriptureDetails.style.color = 'white';
+            scriptureDetails.style.padding = '20px';
+            scriptureDetails.style.borderRadius = '10px';
+            scriptureDetails.textContent = 'No entries found';
+
+            const closeButton = document.createElement('button');
+            closeButton.textContent = 'Close';
+            closeButton.addEventListener('click', function() {
+              overlay.remove();
+            });
+
+            overlay.appendChild(scriptureDetails);
+            overlay.appendChild(closeButton);
+
+            document.body.appendChild(overlay);
+          });        
+
+          const space = createSpace();
+          verseNumber.parentNode.insertBefore(space.cloneNode(true), verseNumber.nextSibling);
+          verseNumber.parentNode.insertBefore(verseButton, verseNumber.nextSibling);
+          verseNumber.style.display = 'none';
+        } else {
+          const verseButton = document.createElement('button');
+          verseButton.style.width = verseButtonWidth;
+          verseButton.style.height = '20px';
+          verseButton.style.border = 'none';
+          verseButton.style.fontSize = '12px';
+          verseButton.textContent = verseNumberText;
+          verseButton.style.background = savedColor || '#191970';
+          verseButton.style.color = 'white';
+          verseButton.style.borderTopLeftRadius = '5px';
+          verseButton.style.borderBottomLeftRadius = '5px';
+          verseButton.style.borderTopRightRadius = '0';
+          verseButton.style.borderBottomRightRadius = '0';
+          verseButton.style.display = 'flex';
+          verseButton.style.alignItems = 'center';
+          verseButton.style.justifyContent = 'center';
+          verseButton.style.padding = '0 5px';
+          verseButton.style.display = 'inline-block';
+          verseButton.style.verticalAlign = 'middle';
+
+          const countButton = document.createElement('button');
+          countButton.style.width = countButtonWidth;
+          countButton.style.height = '20px';
+          countButton.style.border = `1px solid ${savedColor || '#191970'}`;
+          countButton.style.fontSize = '12px';
+          countButton.textContent = `${scriptureCount}`;
+          countButton.style.background = 'white';
+          countButton.style.color = 'black';
+          countButton.style.borderTopLeftRadius = '0';
+          countButton.style.borderBottomLeftRadius = '0';
+          countButton.style.borderTopRightRadius = '5px';
+          countButton.style.borderBottomRightRadius = '5px';
+          countButton.style.display = 'flex';
+          countButton.style.alignItems = 'center';
+          countButton.style.justifyContent = 'center';
+          countButton.style.padding = '0 5px';
+          countButton.style.display = 'inline-block';
+          countButton.style.verticalAlign = 'middle';
+
+          verseButton.addEventListener('click', async function() {
+            const tableView = createTableView(matchingEntries);
+            
+            const overlay = document.createElement('div');
+            overlay.style.position = 'fixed';
+            overlay.style.top = '0';
+            overlay.style.left = '0';
+            overlay.style.width = '100%';
+            overlay.style.height = '100%';
+            overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+            overlay.style.overflowY = 'auto';
+            overlay.style.display = 'flex';
+            overlay.style.alignItems = 'center';
+            overlay.style.justifyContent = 'center';
+            overlay.style.zIndex = '9999';
+            
+            const htmlPaneContent = `
+              <div style="background-color: white; padding: 20px; border-radius: 5px; max-height: 80vh; overflow-y: auto;">
+                <h2>Scripture Details</h2>
+                ${tableView}
+                <button id="closeButton">Close</button>
+              </div>
+            `;
+            
+            overlay.innerHTML = htmlPaneContent;
+            
+            document.body.appendChild(overlay);
+            
+            const closeButton = document.getElementById('closeButton');
+            closeButton.addEventListener('click', function() {
+              overlay.remove();
+            });
+          });        
+
+          countButton.addEventListener('click', async function() {
+            const tableView = createTableView(matchingEntries);
+          
+            const overlay = document.createElement('div');
+            overlay.style.position = 'fixed';
+            overlay.style.top = '0';
+            overlay.style.left = '0';
+            overlay.style.width = '100%';
+            overlay.style.height = '100%';
+            overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+            overlay.style.display = 'flex';
+            overlay.style.alignItems = 'center';
+            overlay.style.justifyContent = 'center';
+            overlay.style.zIndex = '9999';
+          
+            const htmlPaneContent = `
+              <div style="background-color: white; padding: 20px; border-radius: 5px;">
+                <h2>Scripture Details</h2>
+                ${tableView}
+                <button id="closeButton">Close</button>
+              </div>
+            `;
+          
+            overlay.innerHTML = htmlPaneContent;
+          
+            document.body.appendChild(overlay);
+          
+            const closeButton = document.getElementById('closeButton');
+            closeButton.addEventListener('click', function() {
+              overlay.remove();
+            });
           });
-        
-          overlay.appendChild(scriptureDetails);
-          overlay.appendChild(closeButton);
-        
-          document.body.appendChild(overlay);
-        });        
 
-        const space = createSpace();
-        verseNumber.parentNode.insertBefore(space.cloneNode(true), verseNumber.nextSibling);
-        verseNumber.parentNode.insertBefore(verseButton, verseNumber.nextSibling);
-        verseNumber.style.display = 'none';
-      } else {
-        const verseButton = document.createElement('button');
-        verseButton.style.width = verseButtonWidth; // Adjust width as needed
-        verseButton.style.height = '20px'; // Adjust height as needed
-        verseButton.style.border = 'none';
-        verseButton.style.fontSize = '12px';
-        verseButton.textContent = verseNumberText;
-        verseButton.style.background = savedColor || '#191970'; // Use the saved color, or the default
-        verseButton.style.color = 'white';
-        verseButton.style.borderTopLeftRadius = '5px'; // Rounded top-left corner
-        verseButton.style.borderBottomLeftRadius = '5px'; // Rounded bottom-left corner
-        verseButton.style.borderTopRightRadius = '0'; // Normal top-right corner
-        verseButton.style.borderBottomRightRadius = '0'; // Normal bottom-right corner
-        verseButton.style.display = 'flex';
-        verseButton.style.alignItems = 'center';
-        verseButton.style.justifyContent = 'center';
-        verseButton.style.padding = '0 5px'; // Adjust padding as needed to center the content
-        verseButton.style.display = 'inline-block';
-        verseButton.style.verticalAlign = 'middle';
-
-        const countButton = document.createElement('button');
-        countButton.style.width = countButtonWidth; // Adjust width as needed
-        countButton.style.height = '20px'; // Adjust height as needed
-        countButton.style.border = `1px solid ${savedColor || '#191970'}`; 
-        countButton.style.fontSize = '12px';
-        countButton.textContent = `${scriptureCount}`;
-        countButton.style.background = 'white'; // White background
-        countButton.style.color = 'black';
-        countButton.style.borderTopLeftRadius = '0'; // Rounded top-left corner
-        countButton.style.borderBottomLeftRadius = '0'; // Rounded bottom-left corner
-        countButton.style.borderTopRightRadius = '5px'; // Normal top-right corner
-        countButton.style.borderBottomRightRadius = '5px'; // Normal bottom-right corner
-        countButton.style.display = 'flex';
-        countButton.style.alignItems = 'center';
-        countButton.style.justifyContent = 'center';
-        countButton.style.padding = '0 5px'; // Adjust padding as needed to center the content
-        countButton.style.display = 'inline-block';
-        countButton.style.verticalAlign = 'middle';
-
-        verseButton.addEventListener('click', async function() {
-          const tableView = createTableView(matchingEntries);
-          
-          const overlay = document.createElement('div');
-          overlay.style.position = 'fixed';
-          overlay.style.top = '0';
-          overlay.style.left = '0';
-          overlay.style.width = '100%';
-          overlay.style.height = '100%';
-          overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-          overlay.style.overflowY = 'auto'; // Add scrollbar if content exceeds height
-          overlay.style.display = 'flex';
-          overlay.style.alignItems = 'center';
-          overlay.style.justifyContent = 'center';
-          overlay.style.zIndex = '9999';
-          
-          const htmlPaneContent = `
-            <div style="background-color: white; padding: 20px; border-radius: 5px; max-height: 80vh; overflow-y: auto;">
-              <h2>Scripture Details</h2>
-              ${tableView}
-              <button id="closeButton">Close</button>
-            </div>
-          `;
-          
-          overlay.innerHTML = htmlPaneContent;
-          
-          document.body.appendChild(overlay);
-          
-          const closeButton = document.getElementById('closeButton');
-          closeButton.addEventListener('click', function() {
-            overlay.remove();
-          });
-        });        
-        
-        countButton.addEventListener('click', async function() {
-          const tableView = createTableView(matchingEntries);
-        
-          const overlay = document.createElement('div');
-          overlay.style.position = 'fixed';
-          overlay.style.top = '0';
-          overlay.style.left = '0';
-          overlay.style.width = '100%';
-          overlay.style.height = '100%';
-          overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-          overlay.style.display = 'flex';
-          overlay.style.alignItems = 'center';
-          overlay.style.justifyContent = 'center';
-          overlay.style.zIndex = '9999';
-        
-          const htmlPaneContent = `
-            <div style="background-color: white; padding: 20px; border-radius: 5px;">
-              <h2>Scripture Details</h2>
-              ${tableView}
-              <button id="closeButton">Close</button>
-            </div>
-          `;
-        
-          overlay.innerHTML = htmlPaneContent;
-        
-          document.body.appendChild(overlay);
-        
-          const closeButton = document.getElementById('closeButton');
-          closeButton.addEventListener('click', function() {
-            overlay.remove();
-          });
-        });
-        
-
-        const space = createSpace();
-        verseNumber.parentNode.insertBefore(space.cloneNode(true), verseNumber.nextSibling);
-        verseNumber.parentNode.insertBefore(countButton, verseNumber.nextSibling);
-        verseNumber.parentNode.insertBefore(verseButton, verseNumber.nextSibling);
-        verseNumber.style.display = 'none';
+          const space = createSpace();
+          verseNumber.parentNode.insertBefore(space.cloneNode(true), verseNumber.nextSibling);
+          verseNumber.parentNode.insertBefore(countButton, verseNumber.nextSibling);
+          verseNumber.parentNode.insertBefore(verseButton, verseNumber.nextSibling);
+          verseNumber.style.display = 'none';
         }
       });
+    });
+
+    // Wait for all promises to resolve before invoking the callback
+    await Promise.all(promises);
+  }
+  callback();
+}  
+
+
+// window.addEventListener('load', replaceVerseNumbersWithButtons);
+
+// The code will now check every second if the verse ids have been set to display = none.
+
+// This variable is used to help disable to checking every second
+let isReplacing = false;
+let intervalId = null;
+
+function displayIsVisible(isVisible) {
+  const isVisibleDiv = document.createElement('div');
+  isVisibleDiv.textContent = `isVisible: ${isVisible}`;
+  document.body.appendChild(isVisibleDiv);
+}
+
+function checkingButtonsExist() {
+  const verseNumbers = document.querySelectorAll('.verse-number');
+
+  // Check if any verse numbers are visible
+  const isVisible = Array.from(verseNumbers).some(verseNumber => {
+    return window.getComputedStyle(verseNumber).getPropertyValue('display') !== 'none';
+  });
+
+  // displayIsVisible(isVisible);
+
+  // If verse numbers are visible and not currently replacing, execute replaceVerseNumbersWithButtons
+  if (isVisible && !isReplacing) {
+    isReplacing = true;
+
+    // Stop the interval
+    clearInterval(intervalId);
+
+    replaceVerseNumbersWithButtons(() => {
+      // Once replaceVerseNumbersWithButtons completes, reset the flag and start the interval again after a delay
+      isReplacing = false;
+      setTimeout(() => {
+        intervalId = setInterval(checkingButtonsExist, 1000);
+      }, 3000); // Wait for 3 seconds before restarting the interval
     });
   }
 }
 
-window.addEventListener('load', replaceVerseNumbersWithButtons);
+// Start the interval
+intervalId = setInterval(checkingButtonsExist, 1000); // Checking every three seconds
 
-let currentUrl = window.location.href;
 
-function checkUrlChange() {
-  const newUrl = window.location.href;
-  if (newUrl !== currentUrl) {
-    replaceVerseNumbersWithButtons(); // Update extension
-    createSpace();
-    currentUrl = newUrl;
-  }
-}
-
-// Check for URL changes every second (adjust interval as needed)
-setInterval(checkUrlChange, 1000);
