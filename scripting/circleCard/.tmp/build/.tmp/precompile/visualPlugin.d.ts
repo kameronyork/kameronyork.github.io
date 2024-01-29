f8abbd892b36c4cc93bd839b816f3ca71827f6b9
kameronyork <thekameronyork99@gmail.com>
2024-01-28 20:59:03 -0700

    Removing the practice Circle Card visual

--- scripting/circleCard/.tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var circleCardC8A2E7138EF6419EA65CD3052C8B9C68_DEBUG: IVisualPlugin;
-export default circleCardC8A2E7138EF6419EA65CD3052C8B9C68_DEBUG;
