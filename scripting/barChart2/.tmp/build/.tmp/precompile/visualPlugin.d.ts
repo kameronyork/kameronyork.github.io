e54dc6018b6e859b9c67ba1678ec03f5f997974d
kameronyork <thekameronyork99@gmail.com>
2024-01-28 20:56:56 -0700

    removed barchart files

--- scripting/barChart2/.tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var barChart2D1554CAED3B4FAA971478F87181AB79_DEBUG: IVisualPlugin;
-export default barChart2D1554CAED3B4FAA971478F87181AB79_DEBUG;
